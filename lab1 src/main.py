d93405a4db78b504fe130da224eb28a257ac5397
Kenny0711 <kenny103089@gmail.com>
2026-03-10 23:05:05 +0800

    Update model and config for XOR implementation; add result visualization

--- lab1 src/main.py
+++ lab1 src/main.py
@@ -3,14 +3,33 @@ import matplotlib.pyplot as plt
 import json
 from data import gernerate_linear, generate_XOR_easy
 from model import Model
+#show result
+def show_result(x, y, pred_y):
+    import matplotlib.pyplot as plt
+    plt.subplot(1, 2, 1)
+    plt.title('Ground truth', fontsize=18)
+    for i in range(x.shape[0]):
+        if y[i] == 0:
+            plt.plot(x[i][0], x[i][1], 'ro')
+        else:
+            plt.plot(x[i][0], x[i][1], 'bo')
+
+    plt.subplot(1, 2, 2)
+    plt.title('Predict result', fontsize=18)
+    for i in range(x.shape[0]):
+        if pred_y[i] == 0:
+            plt.plot(x[i][0], x[i][1], 'ro')
+        else:
+            plt.plot(x[i][0], x[i][1], 'bo')
+
+    plt.show()
 #load JSON
 with open('config.json','r') as f :
     config = json.load(f)
 print("config:",config)
-
 if config["data"]=="linear":
     x, y = gernerate_linear(n=100)
-else:
+elif config["data"]=="XOR":
     x, y = generate_XOR_easy()
 
 epochs = config["epoch"]
@@ -18,7 +37,8 @@ model = Model(
     input_size=config["input_size"],
     hidden1_size=config["hidden1_size"],
     hidden2_size=config["hidden2_size"],
-    out_size=config["output_size"]
+    out_size=config["output_size"],
+    learning_rate=config["learning_rate"]
 )
 # training
 print("########## Training ##########")
@@ -27,12 +47,8 @@ epoch_list = []
 
 plt.title("Learning Curve")
 for epoch in range(epochs):
-    if config["activation"]=="sigmoid":
-        y_test = model.forward(x)
-        loss = model.backward(x, y)
-    else:
-        y_test = model.forward_no(x)
-        loss = model.backward_no(x, y)
+    y_test = model.forward(x)
+    loss = model.backward(x, y)
     if(epoch % 5000 == 0):
         print(f"epoch: {epoch} loss: {loss}")
     if(epoch % 100 == 0):
@@ -44,11 +60,8 @@ plt.xlabel('epochs', fontsize = 14)
 plt.show()
 # testing
 print("########## Testing ##########")
-if config["activation"]=="sigmoid":
-    y_pred_probability = model.forward(x)
-    print(y_pred_probability)
-else :
-    y_pred_probability = model.forward_no(x)
-    print(y_pred_probability)
+y_pred_probability = model.forward(x)
+print(y_pred_probability)
 y_pred = np.where(y_pred_probability > 0.5, 1, 0)
 print("accuracy: ", sum(1 for i, j in zip(y, y_pred) if i == j) / len(y))
+show_result(x, y, y_pred)
--- lab1 src/model.py
+++ lab1 src/model.py
@@ -2,7 +2,7 @@ import numpy as np
 import json
 #model
 class Model:
-    def __init__(self,input_size=2,hidden1_size=5,hidden2_size=5,out_size=1,learning_rate=0.001):
+    def __init__(self,input_size=2,hidden1_size=5,hidden2_size=5,out_size=1,learning_rate=0.001,activation: str="sigmoid"):
         #implemnt the model
         self.input_size=input_size
         self.hidden1_size=hidden1_size
@@ -12,64 +12,62 @@ class Model:
         self.weight1=np.random.rand(self.input_size,self.hidden1_size)
         self.weight2=np.random.rand(self.hidden1_size,self.hidden2_size)
         self.weight3=np.random.rand(self.hidden2_size,self.output_size)
-
-        self.bias1=0.001
-        self.bias2=0.001
-        self.bias3=0.001
+        #每個neural都應該要有一個bias
+        self.bias1=0.01
+        self.bias2=0.01
+        self.bias3=0.01
 
         self.learning_rate=learning_rate
-
+        self.activation=activation
+    #activation function
     def sigmoid(self,x):
         return 1/(1+np.exp(-x))
     def sigmoid_derivative(self, x):
         return x*(1-x)
-
-    def forward(self, x):
-        self.hidden1_output=self.sigmoid(np.dot(x,self.weight1)+self.bias1)
-        self.hidden2_output=self.sigmoid(np.dot(self.hidden1_output,self.weight2)+self.bias2)
-        self.y=self.sigmoid(np.dot(self.hidden2_output,self.weight3)+self.bias3)
+    def ReLu(self,x):
+        return np.maximum(0,x)
+    def Relu_derivative(self,x):
+        return (x>0).astype(float)
+    def none(self,x):
+        return x
+    def forward(self,x):
+        #y=wx+b
+        if self.activation=="sigmoid":
+            act=self.sigmoid
+        elif self.activation=="none":
+            act=self.none
+        elif self.activation=="ReLu":
+            act=self.ReLu
+        self.hidden1_output=act(np.dot(x,self.weight1)+self.bias1)
+        self.hidden2_output=act(np.dot(self.hidden1_output,self.weight2)+self.bias2)
+        self.y=act(np.dot(self.hidden2_output,self.weight3)+self.bias3)
         return self.y
     def backward(self,x,y_gt):
+        if self.activation=="sigmoid":
+            act=self.sigmoid_derivative
+        elif self.activation=="none":
+            act=np.ones_like(x)
+        elif self.activation=="ReLu":
+            act=self.ReLu_derivative
         #gt is the ground truth
         y_error=self.y-y_gt
-        y_delta=y_error*self.sigmoid_derivative(self.y)
+        y_delta=y_error*act(self.y)
         #update weights
         self.weight3-=self.learning_rate*np.dot(self.hidden2_output.T,y_delta)
         self.bias3-=self.learning_rate*y_delta
 
         #hidden2 error
         hidden2_error=np.dot(y_delta,self.weight3.T)
-        hidden2_delta=hidden2_error*self.sigmoid_derivative(self.hidden2_output)
+        hidden2_delta=hidden2_error*act(self.hidden2_output)
         #update weights level2
         self.weight2-=self.learning_rate*np.dot(self.hidden1_output.T,hidden2_delta)
         self.bias2-=self.learning_rate*hidden2_delta
 
         #hidden1 error
         hidden1_error=np.dot(hidden2_delta,self.weight2.T)
-        hidden1_delta=hidden1_error*self.sigmoid_derivative(self.hidden1_output)
+        hidden1_delta=hidden1_error*act(self.hidden1_output)
         #update weights level1
         self.weight1-=self.learning_rate*np.dot(x.T,hidden1_delta)
         self.bias1-=self.learning_rate*hidden1_delta
-
         return (1/2)*np.sum((self.y-y_gt)**2)
-    #不使用sigmoid
-    def forward_no(self,x):
-      self.hidden1_output=np.dot(x,self.weight1)+self.bias1
-      self.hidden2_output=np.dot(self.hidden1_output,self.weight2)+self.bias2
-      self.y=np.dot(self.hidden2_output,self.weight3)+self.bias3
-      return self.y
-    def backward_no(self,x,y_gt):
-      y_error=self.y-y_gt
-      y_delta=y_error
-      self.weight3-=self.learning_rate*np.dot(self.hidden2_output.T,y_delta)
-      self.bias3-=self.learning_rate*y_delta
-      hidden2_error=np.dot(y_delta,self.weight3.T)
-      hidden2_delta=hidden2_error
-      self.weight2-=self.learning_rate*np.dot(self.hidden1_output.T,hidden2_delta)
-      self.bias2-=self.learning_rate*hidden2_delta
-      hidden1_error=np.dot(hidden2_delta,self.weight2.T)
-      hidden1_delta=hidden1_error
-      self.weight1-=self.learning_rate*np.dot(x.T,hidden1_delta)
-      self.bias1-=self.learning_rate*hidden1_delta
-      return (1/2)*np.sum((self.y-y_gt)**2)
     
